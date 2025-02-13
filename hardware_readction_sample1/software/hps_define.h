09531e51f8e711ed4d12a5b2c729ad35164ecc05
Chaitali Sathe <54367575+chaitalisathe@users.noreply.github.com>
2025-02-12 18:17:24 -0600

    Add files via upload

---
+++ hardware_readction_sample1/software/hps_define.h
@@ -0,0 +1,13 @@
+#ifndef hps_define
+#define hps_define
+
+
+
+#define SIZE  432 // size of bitstream {can be asked as an input from user}
+#define TEST_VEC_SIZE  3 //input vector size - can be asked as an input
+#define TEST_OUT_SIZE  2 // output vector size
+#define NUM_TEST_VEC  32 // number of test vectors
+
+
+
+#endif
