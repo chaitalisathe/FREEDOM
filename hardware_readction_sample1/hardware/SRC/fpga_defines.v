f1cd42e939f1f9f72b98ce81117c263e5301ec68
Chaitali Sathe <54367575+chaitalisathe@users.noreply.github.com>
2025-02-12 18:07:34 -0600

    Create fpga_defines.v

---
+++ hardware_readction_sample1/hardware/SRC/fpga_defines.v
@@ -0,0 +1,9 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Wed Feb 12 15:59:47 2025
+//-------------------------------------------
+`define ENABLE_TIMING 1
+
