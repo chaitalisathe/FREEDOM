97a22b4fe8a42dc7d6fd1a1641268f2c963a72fd
Chaitali Sathe <54367575+chaitalisathe@users.noreply.github.com>
2025-02-13 11:27:02 -0600

    Update asic_fpga_myadder.v

--- hardware_readction_sample1/hardware/asic_fpga_myadder.v
+++ hardware_readction_sample1/hardware/asic_fpga_myadder.v
@@ -1,18 +1,19 @@
-module asic_fpga_myadder(
-input [0:0] A, 
-input [0:0] B, 
-input [0:0] CI, 
-output [0:0] CO, 
-output [0:0] SUM,
-input      f_prog_clk,
-input      f_reset,
-input      f_ccff_head,
-output     f_ccff_tail,
-input 		clk
-);
+module asic_fpga_myadder( A, B, CI, CO, SUM, f_op_clk, f_prog_clk, f_reset, f_ccff_head, f_ccff_tail);
+input  	  A; 
+input 	  B; 
+input  	  CI; 
+output 	  CO; 
+output 	  SUM;
+input     f_prog_clk;
+input     f_reset;
+input     f_ccff_head;
+output    f_ccff_tail;
+input 	  f_op_clk;
 
 
-  assign SUM = A ^ B ^ CI;
+
+
+assign SUM = A ^ B ^ CI;
   
 
 //fpga fpga_inst (.B(B), .CI(CI), .A(A), .CO(CO));
@@ -22,7 +23,7 @@ wire [0:31] gfpga_pad_GPIO_PAD;
 fpga_top FPGA_DUT (.prog_clk(f_prog_clk),
 .set(1'b0),
 .reset(f_reset),
-.clk(clk),
+.clk(f_op_clk),
 .gfpga_pad_GPIO_PAD(gfpga_pad_GPIO_PAD[0:31]),
 .ccff_head(f_ccff_head),
 .ccff_tail(f_ccff_tail));
