3db2bcb4044ef05c6054e13ac9fc229c09899415
Chaitali Sathe <54367575+chaitalisathe@users.noreply.github.com>
2025-02-13 14:08:24 -0600

    Update user_defined_parameters.sv

--- hardware_readction_sample1/user_defined_parameters.sv
+++ hardware_readction_sample1/user_defined_parameters.sv
@@ -1,7 +1,7 @@
 // parameters file
 parameter BITSTREAM_LENGTH = 'd432;
 parameter LATENCY = 1;
-parameter INPUT_LENGTH = 'd3 ;
-parameter OUTPUT_LENGTH = 'd2 ;
+parameter INPUT_LENGTH = 'd3 ; // Input bitwidth
+parameter OUTPUT_LENGTH = 'd2 ; // output bitwidth
 
  
