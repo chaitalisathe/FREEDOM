8dcf9ce677070ad657d0ecfb1f7c8fbb5782e47d
Chaitali Sathe <54367575+chaitalisathe@users.noreply.github.com>
2025-02-13 14:00:01 -0600

    Update myadder.v

--- hardware_readction_sample1/hardware/myadder.v
+++ hardware_readction_sample1/hardware/myadder.v
@@ -3,23 +3,21 @@
 // This file has pragmas included for redaction process
 // comment out pragma for RTL simulation.
 //-----------------------------------------------------
-module myadder_redacted (
-    B,
-    CI,
-    A,
-    CO
-);
-  input  A; // Input a
-  input  B; // Input b
-  input  CI; // Input cin
+module myadder( A, B, CI, CO, SUM );
+  input A; // Input a
+  input B;// Input b
+  input CI; // Input cin
   output CO; // Output carry
-  output SUM; // Output sum
-
-  assign SUM = A ^ B ^ CI;
+  output SUM ;// Output sum
+  
+  
+assign SUM = A ^ B ^ CI;
   
 startpragma
   assign CO = (A & B) | (A & CI) | (B & CI); 
 endpragma
   
 endmodule
+
+
 
