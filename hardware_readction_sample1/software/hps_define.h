4d6ba8d3b7d109f2ca407f5a6bee81f6c51f6d4f
Chaitali Sathe <54367575+chaitalisathe@users.noreply.github.com>
2025-02-18 17:23:08 -0600

    Update hps_define.h

--- hardware_readction_sample1/software/hps_define.h
+++ hardware_readction_sample1/software/hps_define.h
@@ -7,7 +7,7 @@
 #define TEST_VEC_SIZE  3 //input vector size - can be asked as an input
 #define TEST_OUT_SIZE  2 // output vector size
 #define NUM_TEST_VEC  32 // number of test vectors
-
+#define MAX_BITSTREAM_INTERATIONS 10 // Maximum number of bitstreams to try
 
 
 #endif
