59af352126f536496fc5f1de30d8d8a4e53d2c4d
Chaitali Sathe <54367575+chaitalisathe@users.noreply.github.com>
2025-02-18 17:22:42 -0600

    Update hps_fpga_test.c

--- hardware_readction_sample1/software/hps_fpga_test.c
+++ hardware_readction_sample1/software/hps_fpga_test.c
@@ -313,16 +313,18 @@ int readTV(void)
 			test_vectors[j][i] = ch;
 			//printf("value of current test input bit : %c \n", test_vectors[j][i]);
 			i++;
+			
+				if (i == test_vec_size)
+				{
+					//printf("test vector copying : %d \n", j);
+					j++;
+					i= 0;
+				}
+			
+			
+			
 			}
-		else 
-		{
-			if (ch == '\n')
-			{
-				//printf("test vector copying : %d \n", j);
-				j++;
-				i= 0;
-			}
-		}
+		
 	} 
 	 
 	fclose(file1);
@@ -334,7 +336,7 @@ int readTV(void)
 			printf("Error in opening golden output file \n");
 			return 1;
 		}
-	 while (!feof(file2))
+	while (j < (num_test_vec))
 	{
 
 		out_ch = fgetc(file2);
@@ -348,20 +350,19 @@ int readTV(void)
 			out_vectors[j][i] = out_ch;
 			//printf("value of current output bit : %c \n", out_vectors[j][i]);
 			i++;
+				if (i == test_out_size)
+				{
+					//printf("test output copying : %d \n", j);
+					j++;
+					i= 0;
+				}	
+			
+			
 			}
-		else 
-		{
-			if (out_ch == '\n')
-			{
-				//printf("column copying : %d \n", j);
-				j++;
-				i= 0;
-			}
-		}
+		
 	}  
 	fclose(file2);  
 	
-
 	printf("leaving test vectors:\n" );
 	return 0;	
 }
@@ -382,7 +383,7 @@ bool compare_results(void)
 	int i = 0;
 	char out_current[NUM_TEST_VEC][TEST_OUT_SIZE];
 	printf("Comparing current output with golden output:\n" );
-	while (!feof(file2) ) 
+	while (j < (num_test_vec))
 		{
 			out_ch = fgetc(file2);
 			//printf("value of current out_ch : %c\n", out_ch);
@@ -394,23 +395,17 @@ bool compare_results(void)
 			out_current[j][i] = out_ch;
 			//printf("value of current output bit : %c\n", out_current[j][i]);
 			i++;
+			
+			if (i == test_out_size)
+				{
+					//printf("test output copying : %d \n", j);
+					j++;
+					i= 0;
+				}	
+				 
+			
 			}
-		else 
-		{
-			if (out_ch == '\n')
-			{
-				//printf("column copying : %d \n", j);
-				j++;
-				i= 0;
-			}
-		}	
-			
-			
-			
-			
-			
-			
-			
+		
 
 				
 		}
@@ -451,7 +446,7 @@ int main(void)
 	int output_iter = test_out_size/ bus_size;
 	char sub[bus_size]; 
 	//char test_io[bus_size];
-	int total_iterations = 1;// c = size;
+	int total_iterations = MAX_BITSTREAM_INTERATIONS;
 	bool result = true;
     char* bitstream_ptr = malloc((total_iterations * size) * sizeof(char));		
 	char ch_0 = '0';
@@ -464,6 +459,8 @@ int main(void)
 	memory_map(); // Done only once
 	int bitstream_iteration = 0;
 	bool found_match = false;
+	int timeout_max = 1000; // Ends program if matching bitstream is re-generated timeout times.
+	int timeout = 0;
 	int copy = 0;
 	readTV();
 	unsigned int p = 0;
@@ -474,7 +471,8 @@ int main(void)
 		// state 0 - Generate bitstream
 		hamming_distance = 0;
 		generate_bitstream(); // Fuzzer generates the bitstream and stores into a fabric_bitstream.bit file
-		read_golden_bitstream(); // Bitstream is read from file fabric_bistream.bit
+		//read_golden_bitstream(); // fabric_bistream_golden.bit 
+		read_bitstream(); // fabric_bitstream_sample.bit
 		//printf("out of read loop \n");
 		//printf("calling check_bitstream function");
 		for( i = 0; i < bitstream_iteration; i++) 
@@ -496,21 +494,26 @@ int main(void)
 			if (match == size)	
 			{
 				found_match = true;
+				timeout++;
 				printf("foundmatch , restarting while loop \n");
 				break;
 			}
 			else
+			{
 			found_match = false;
+			timeout = 0;
+			}
 				
 		}
 		
 		if (found_match == false)
 		// store bitstream in data
 		{	
+		
 			for ( j = 0; j < size; j++)
 				 bitstream_ptr[bitstream_iteration * size + j] = bitstream[j];
 			
-			printf("store bitstream in data \n");
+			//printf("store bitstream in data \n");
 			//bitstream_iteration ++;
 		
 		
@@ -674,8 +677,8 @@ int main(void)
 					
 						*(axi_pio_ptr) =  bits; // send 32 bits of bitstream LSB [0+i*32 : 0+i*32+31]
 						*(lw_pio_ptr)  =  FLAGH2F_5; // send flag for sending 32 bits of bitstream
-						printf("Flag H5 is raised by HPS- Sending 32 bits of an input \n")	;
-						printf("Waiting for FLAG F5 from FPGA- FPGA neeeds to copy 32bits of input on register \n");
+						//printf("Flag H5 is raised by HPS- Sending 32 bits of an input \n")	;
+						//printf("Waiting for FLAG F5 from FPGA- FPGA neeeds to copy 32bits of input on register \n");
 						while(1)	// wait until FPGA is ready to receive next bitstream bits
 							{
 								if (*(lw_pio_read_ptr) == FLAGF2H_5)
@@ -701,53 +704,7 @@ int main(void)
 							}
 					
 
-							/* while(1)	// wait until FPGA finishes testing
-							{
-								if (*(lw_pio_read_ptr) == FLAGF2H_7) // output is ready
-								{
-									printf("Received output from FPGA for inputs \n");
-									printf("output %d : %d\n",z, *(axi_pio_read_ptr)) ;
-									// write onto text file
-									//fprintf(file_out, "%d\n", *(axi_pio_read_ptr));
-									// reading outputs 32 bits at a time.
-									
-									
-									n = *(axi_pio_read_ptr);
-									t = 0;
-									f= 1;
-									i=0;
-											while (n > 0) {
-												// storing remainder in binary array
-														if(n%2 == 0){
-															fprintf(file_out, "%c", ch_0);
-															//printf("writing %c in output file", ch_0 );
-															i++;
-														}
-														else{
-															fprintf(file_out, "%c", ch_1);
-															//printf("writing %c in output file", ch_1 );
-															i++;
-														}
-													
-															n = n / 2;
-				
-														}
-															
-									
-											for(j = i ; j< test_out_size;j++)
-													{
-													fprintf(file_out, "%c", ch_0);
-													//printf("writing %c in output file", ch_0 );
-													}
-									
-											fprintf(file_out, "\n");
-									//printf(" Flag F7 is raised from FPGA and finished testing \n");
-									*(lw_pio_ptr) = FLAGH2F_8 ; // Read all bits of output ready to read next bits 
-									//printf(" Flag H8 is raised from HPS and HPS has read the output\n");
-									break;
-								}
-								
-							} */
+							
 							
 							while(1)	// wait until FPGA finishes testing
 							{
@@ -1063,13 +1020,13 @@ int main(void)
 		bitstream_iteration ++;
 		printf("Current bitstream iteration %d \n",bitstream_iteration);
 		result = compare_results();
-				if (result == true)
+		if (result == true)
 		printf("Leaving comparison, result is TRUE \n");
-	else
+		else
 		printf("Leaving comparison, result is FALSE \n");
 		
 		
-/* 		printf("Leaving comparison \n");
+		//printf("Leaving comparison \n");
 		char filename[50];
 		if (result == true)
 			{
@@ -1097,8 +1054,16 @@ int main(void)
 			}
 	
 	
-		printf("Hamming distance for current bitstream = %d \n", hamming_distance); */
+		printf("Hamming distance for current bitstream = %d \n", hamming_distance);
 		}
+		
+		if (timeout >= timeout_max)
+		{
+			printf("Repeatative bitstream generated ");
+			break;
+		}
+		
+		
 		
 		}
 	
